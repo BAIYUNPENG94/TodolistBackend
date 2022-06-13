07140b14e70018583cf6ea1595988eff7f4f6f99
Yunpeng.Bai <baiyunpeng@yahoo.com>
2022-06-13 23:57:09 +0900

    a
    a

--- oracleNodejsExample.js
+++ oracleNodejsExample.js
@@ -5,29 +5,29 @@ async function run() {
 
 	try {
 		//This needs to be verified tonight.
-		connection = await oracledb.getConnection({ user: "system", password: "taiho", connectionString: "192.168.2.120:1521/XE"});
+		connection = await oracledb.getConnection({ user: "system", password: "taiho", connectionString: "192.168.2.125:1521/XE"});
 
 		console.log("connect SUCCESS");
 
-		await connection.execute(`begin
-			execute immediate 'drop table todoitem';
-			exception when others then if sqlcode <> -942 then raise; end if;
-			end;`);
+		//await connection.execute(`begin
+		//	execute immediate 'drop table todoitem';
+		//	exception when others then if sqlcode <> -942 then raise; end if;
+		//	end;`);
 
-		await connection.execute(` create table todoitem (
-				id number generated always as identity,
-				description varchar2(4000),
-				creation_ts timestamp with time zone default current_timestamp,
-				done number(1, 0),
-				primary key (id))`);
+		//await connection.execute(` create table todoitem (
+		//		id number generated always as identity,
+		//		description varchar2(4000),
+		//		creation_ts timestamp with time zone default current_timestamp,
+		//		done number(1, 0),
+		//		primary key (id))`);
 		
 		const sql = 'insert into todoitem (description, done) values (:1, :2)';
 
 		const rows = [
 			["task 1", 1],
-			["task 2", 1],
+			["task 2", 0],
 			["task 3", 1],
-			["task 4", 1],
+			["task 4", 0],
 			["task 5", 1]
 		];
 
