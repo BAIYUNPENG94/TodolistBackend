3d0c1a665d4e3ed608ccd9bd57880ee45aea72dc
Yunpeng.Bai <49668195+BAIYUNPENG94@users.noreply.github.com>
2022-06-15 11:57:27 +0900

    d
    d

--- todoApp/oracleFuncs.js
+++ todoApp/oracleFuncs.js
@@ -5,14 +5,16 @@ function Todo() {
         connection.acquire((err, conn) => {
             if (err) {
                 console.log('Problem 004: ', err.message);
+            } else {
+                conn.execute(`SELECT * FROM TODOITEM`, (err, result) => {
+                    if (err) {
+                        console.log('Problem 0041: ', err.message);
+                    } else {
+                        console.log('GET SUCCESS: ', result)
+                        res.send(result);
+                    }
+                })
             }
-            conn.execute('SELECT * FROM TODOITEM', (err, result) => {
-                if (err) {
-                    console.log('Problem 0041: ', err.message);
-                } else {
-                    console.log('GET SUCCESS: ', result)
-                }
-            })
         })
     };
 
@@ -20,14 +22,33 @@ function Todo() {
         connection.acquire((err, conn) => {
             if (err) {
                 console.log('Problem 004: ', err.message);
+            } else {
+                conn.execute(`SELECT * FROM TODOITEM WHERE ID = :ID`, [id], (err, result) => {
+                    if (err) {
+                        console.log('Problem 0042: ', err.message);
+                    } else {
+                        console.log('GET SUCCESS: ', result)
+                        res.send(result);
+                    }
+                })
             }
-            conn.execute('SELECT * FROM TODOITEM', (err, result) => {
-                if (err) {
-                    console.log('Problem 0041: ', err.message);
-                } else {
-                    console.log('GET SUCCESS: ', result)
-                }
-            })
+        })
+    };
+
+    this.create = function(todo, res) {
+        connection.acquire((err, conn) => {
+            if (err) {
+                console.log('Problem 004: ', err.message);
+            } else {
+                conn.execute(`INSERT INTO TODOITEM SET :TODO`, [todo], (err, result) => {
+                    if (err) {
+                        console.log('Problem 0043: ', err.message);
+                    } else {
+                        console.log('GET SUCCESS: ', result)
+                        res.send(result);
+                    }
+                })
+            }
         })
     };
 }
